06e48587629b91f4e104cc5ac2552acb8ef7d23c
Hanif1201 <adeyemihanif@gmail.com>
2023-11-17 22:02:48 +0100

    faq section

---
+++ src/components/DownloadApp.jsx
@@ -0,0 +1,19 @@
+import "./Styles/DownloadApp.css";
+
+const DownloadApp = () => {
+  return (
+    <div className="downloadApp__section">
+      <div className="downloadApp__section__content">
+        <p className="download__section__paragraph">
+          Join the exclusive set of individuals we are currently making their
+          dream remote job a reality.
+        </p>
+        <button className="download__section__button">
+          Download JobMingle App Now
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DownloadApp;
